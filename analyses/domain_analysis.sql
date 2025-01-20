1998dd8de6dd6b7add627437007d13e7b81528a1
Gisela Chen <giselacchen@gmail.com>
2025-01-20 17:18:39 -0500

    review_analysis_queries

--- analyses/domain_analysis.sql
+++ analyses/domain_analysis.sql
@@ -8,22 +8,34 @@ Questions to Answer:
 
 */
 
+/* Added a CTE to unnest domain and perform string cleaning - trim outside of the join clause. */
+with urns_with_domains as (
 select
-  -- entity_with_domains.entity_type
-  -- , trim(both '"' from domain_flat.domain_urn) as domain_urn
-  json_extract_string(domain_details.entity_details, '$.name') as domain_name
-  , json_extract_string(domain_details.entity_details, '$.description') as domain_description
-  , count(distinct entity_with_domains.urn) as entity_count
+    entity_with_domains.urn as urn
+--    , entity_with_domains.entity_type
+    , trim(both '"' from domain_flat.domain_urn) as domain_urn
 from
-  stg_datahub_entities as entity_with_domains,
-  unnest(json_extract_string(entity_with_domains.domains, '$.domains')::string[]) as domain_flat(domain_urn)
-left join
-  stg_datahub_entities as domain_details
-  on trim(both '"' from domain_flat.domain_urn) = domain_details.urn
+    stg_datahub_entities as entity_with_domains,
+    unnest(json_extract_string(entity_with_domains.domains, '$.domains')::string[]) as domain_flat(domain_urn)
 where
-  entity_with_domains.domains is not null
+    entity_with_domains.domains is not null
+)
+
+select
+--  urns_with_domains.entity_type,
+    json_extract_string(domain_details.entity_details, '$.name') as domain_name
+    , json_extract_string(domain_details.entity_details, '$.description') as domain_description
+    , count(distinct urns_with_domains.urn) as distinct_urn_count     
+from 
+    urns_with_domains
+left join
+    stg_datahub_entities as domain_details
+    on urns_with_domains.domain_urn = domain_details.urn
 group by 1, 2
-order by 2 desc
+/* the order by clause below orders the result by domain description and not entity count and 
+generates the wrong result. The domain associated with most dataset/dashboard should be 
+Finance with 285 counts */
+order by 3 desc
 limit 1
 ;
 
--- analyses/ownership_analysis.sql
+++ analyses/ownership_analysis.sql
@@ -8,21 +8,41 @@ Questions to Answer:
 
 */
 
-select 
-    a.entity_type,
-    -- c.owner_urn,
-    json_extract_string(b.entity_details, '$.username') username,
-    json_extract_string(b.entity_details, '$.title') as title ,
-    count(distinct a.urn) as cnt
-from stg_datahub_entities a,
-     unnest(json_extract(owners, '$.owners')::json[]) c(owner_urn)
-LEFT OUTER JOIN 
-stg_datahub_entities b
-on json_extract_string(c.owner_urn, '$.owner')=b.urn
-group by 1,2,3--,4
-order by 2,1
-;
+/* When assign ownership, each user can be a business or a technical
+   owner in which the current query result doesn't differentiate. 
+   The query can be updated to include additional column for
+   differentiation */
 
+/* Added a CTE to unnest owners first */
+with urns_with_owners as (
+select
+    entity_with_owners.urn as urn
+    , entity_type
+    , json_extract_string(owner_flat.owner_urn, '$.owner') as owner_urn
+    --, lower(json_extract_string(owner_flat.owner_urn, '$.type')) as owner_type
+from 
+    stg_datahub_entities as entity_with_owners,
+    unnest(json_extract(owners, '$.owners')::json[]) as owner_flat(owner_urn)
+where 
+    entity_with_owners.owners is not null
+)
+
+select 
+    urns_with_owners.entity_type
+    --, urns_with_owners.owner_type
+    , json_extract_string(user_details.entity_details, '$.username') as username
+    , json_extract_string(user_details.entity_details, '$.title') as title
+    , count(distinct urns_with_owners.urn) as distinct_urn_count
+from 
+    urns_with_owners
+/* Use lower case left join for consistency. Updated table alias with descriptive 
+   terms for readability and consistent */
+left join 
+    stg_datahub_entities as user_details
+    on urns_with_owners.owner_urn = user_details.urn
+group by 1, 2, 3--, 4
+order by 2, 1;
+;
 
 /*
 
